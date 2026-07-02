7bba054778990a2920e297e04b9e04f275c0315f
melkadi84-blip <melkadi84@users.noreply.github.com>
2026-07-02 12:37:28 +0300

    Enforce DATABASE_URL check in settings.py to ensure the application always connects to the online database even on localhost

--- crafts_project/settings.py
+++ crafts_project/settings.py
@@ -95,19 +95,14 @@ WSGI_APPLICATION = 'crafts_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if dj_database_url and os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+if not os.environ.get('DATABASE_URL'):
+    raise ValueError("The DATABASE_URL environment variable is not set. Please define it in your .env file.")
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
