40ff64c500444e52823c3a8ba2fb5f2cd28f90f3
melkadi84-blip <melkadi84@users.noreply.github.com>
2026-07-02 12:43:57 +0300

    Serve media files locally regardless of DEBUG settings

--- crafts_project/urls.py
+++ crafts_project/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('owners/', include('owners.urls')),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG or not os.environ.get('RENDER'):
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
